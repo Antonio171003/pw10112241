849cc7362c176ace859b1e1f087d340eef2f9520
Antonio17 <luis.antonio.171003@gmail.com>
2024-05-16 10:49:22 -0700

    Borrar y registrar clientes

---
+++ proyecto/backend/proyecto/frontend/Tienda/src/views/ClientesCreateView.vue
@@ -0,0 +1,101 @@
+<template>
+
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-heard">
+                <h4>Agregar Cliente</h4>
+                <div v-if="mensaje == 1" class="alert alert-success" role="alert">
+                    Datos guardados con exito.
+                </div>
+                <div v-if="mensaje == 2" class="alert alert-error" role="alert">
+                    No se pudo guardar los datos.
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="mb-3">
+                    Id
+                    <input type="text" class="form-control" v-model="model.cliente.id">
+                </div>
+                <div class="mb-3">
+                    Nombre
+                    <input type="text" class="form-control" v-model="model.cliente.nombre">
+                </div>
+                <div class="mb-3">
+                    Apellido
+                    <input type="text" class="form-control" v-model="model.cliente.apellido">
+                </div>
+                <div class="mb-3">
+                    Dirrecion
+                    <input type="text" class="form-control" v-model="model.cliente.direccion">
+                </div>
+                <div class="mb-3">
+                    Telefono
+                    <input type="text" class="form-control" v-model="model.cliente.telefono">
+                </div>
+                <div class="mb-3">
+                    RFC
+                    <input type="text" class="form-control" v-model="model.cliente.rfc">
+                </div>
+                <div class="mb-3">
+                    CURP
+                    <input type="text" class="form-control" v-model="model.cliente.curp">
+                </div>
+                <div class="mb-3">
+                    CP
+                    <input type="text" class="form-control" v-model="model.cliente.cp">
+                </div>
+                <div class="mb-3">
+                    <button class="btn btn-primary" @click="guardarCliente()">Guardar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+import axios from 'axios';
+export default{
+    name: "ClientesCreate",
+    data(){
+        return{
+            mensaje: 0,
+            model:{
+                cliente:{
+                    id: '',
+                    nombre: '',
+                    apellido: '',
+                    direccion: '',
+                    telefono: '',
+                    rfc: '',
+                    curp: '',
+                    cp: ''
+                }
+            }
+        }
+    },
+    methods:{
+        guardarCliente(){
+            axios.post('http://localhost:3000/api/clientes', this.model.cliente)
+            .then(res => {
+                if(res.data.affectedRows == 1){ // Si insertamos un registro
+                    this.model.cliente = { // Limpiamos los cuadros de texto
+                        id: '',
+                        nombre: '',
+                        apellido: '',
+                        direccion: '',
+                        telefono: '',
+                        rfc: '',
+                        curp: '',
+                        cp: ''
+                    }
+                    this.mensaje = 1
+                }
+                else{
+                    this.mensaje = 2
+                }
+            })
+        }
+    }
+}
+</script>
